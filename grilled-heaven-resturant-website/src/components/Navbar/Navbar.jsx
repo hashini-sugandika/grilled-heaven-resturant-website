395b1834985f79a6ce58ae6ed702c424c6518539
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2023-12-29 21:26:18 +0530

    navigation bar create

--- grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
+++ grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className='app__navbar__logo'>
+      <img src="../../assets/logo.png" alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#menu">Menu</a></li>
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
+    </ul>
+    
+  </nav>
 );
 
-export default Navbar;
+
+export default Navbar
