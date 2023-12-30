fc872220fe916b38b9cfc01f1d75ece9838cf32d
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2023-12-30 11:30:04 +0530

    navbar done

--- grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
+++ grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
@@ -15,7 +15,6 @@ const Navbar = () => {
         <li className="p__opensans"><a href="#home">Home</a></li>
         <li className="p__opensans"><a href="#about">About</a></li>
         <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
         <li className="p__opensans"><a href="#contact">Contact</a></li>
       </ul>
       <div className="app__navbar-login">
@@ -32,7 +31,6 @@ const Navbar = () => {
               <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
               <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
               <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
-              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
               <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
             </ul>
           </div>
