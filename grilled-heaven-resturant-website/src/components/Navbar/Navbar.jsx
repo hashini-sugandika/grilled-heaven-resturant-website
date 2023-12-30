8596569b0ee24485861bc3bd182fde6e9f4db459
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2023-12-30 11:30:22 +0530

    navbar completed

--- grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
+++ grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import images from '../../constants/images';
+import {images} from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
