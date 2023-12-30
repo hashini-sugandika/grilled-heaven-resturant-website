e50fe4711b440109b4838fb5edc5426e143c88e4
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2023-12-30 12:23:25 +0530

    Header section completed

--- grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
+++ grilled-heaven-resturant-website/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import {images} from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -9,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src="" alt="app__logo" />
+        <img src="" alt="" />
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans"><a href="#home">Home</a></li>
